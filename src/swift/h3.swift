a928f0306b61b405eb2f2c64a6c5882c27abb2b5
zloysergunya <zloysergunya.lolz@gmail.com>
2023-09-21 00:09:49 +0700

    Fix legacy bugs

--- src/swift/h3.swift
+++ src/swift/h3.swift
@@ -1,30 +1,23 @@
-// h3.swift
-// 
-// Created by Michael Chartier on 11/2/2020
-// Copyright © Michael Chartier. All rights reserved.
-
-import Foundation
 import CoreLocation
-
 
 extension CLLocationCoordinate2D {
     // Find the H3 cell index for a given set of 2D coordinates
-    public func h3CellIndex( resolution: Int32 ) -> UInt64 {
+    public func h3CellIndex(resolution: Int32) -> UInt64 {
         let lat = degsToRads(latitude)
         let lon = degsToRads(longitude)
         var location = GeoCoord(lat: lat, lon: lon)
-        let index = geoToH3(&location, 6)
+        let index = geoToH3(&location, resolution)
+        
         return index
     }
+    
     // Find the neighbor cells for a given set of 2D coordinates and ring size
-    public func h3Neighbors( resolution: Int32, ringLevel: Int32 ) -> [H3Index]
-    {
+    public func h3Neighbors(resolution: Int32, ringLevel: Int32) -> [H3Index] {
         let index = h3CellIndex(resolution: resolution)
         let count = Int(maxKringSize(ringLevel))
         var neighbors = Array(repeating: H3Index(), count: count)
         kRing(index, ringLevel, &neighbors);
+        
         return neighbors
     }
 }
-
-

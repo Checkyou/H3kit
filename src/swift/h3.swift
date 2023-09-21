cc720b191c1250b04fe196072f093700a3210b3e
zloysergunya <zloysergunya.lolz@gmail.com>
2023-09-21 17:05:58 +0700

    Add UInt64 to hexagon

--- src/swift/h3.swift
+++ src/swift/h3.swift
@@ -21,3 +21,20 @@ extension CLLocationCoordinate2D {
         return neighbors
     }
 }
+
+extension UInt64 {
+    public func h3ToHexagon() -> [CLLocationCoordinate2D] {
+        var locations = GeoBoundary()
+        h3ToGeoBoundary(self, &locations)
+        
+        return [
+            .init(latitude: locations.verts.0.lat, longitude: locations.verts.0.lon),
+            .init(latitude: locations.verts.1.lat, longitude: locations.verts.1.lon),
+            .init(latitude: locations.verts.2.lat, longitude: locations.verts.2.lon),
+            .init(latitude: locations.verts.3.lat, longitude: locations.verts.3.lon),
+            .init(latitude: locations.verts.4.lat, longitude: locations.verts.4.lon),
+            .init(latitude: locations.verts.5.lat, longitude: locations.verts.5.lon),
+            .init(latitude: locations.verts.0.lat, longitude: locations.verts.0.lon)
+        ]
+    }
+}
